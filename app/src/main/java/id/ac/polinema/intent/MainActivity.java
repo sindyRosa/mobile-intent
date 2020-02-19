f70af141af127cdb5a35a07dcabcc401cb18e86e
sindyRosa <sindyros123@gmail.com>
2020-02-19 18:25:20 +0700

    Explicit Intent

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -1,6 +1,10 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void handledExplisitIntent(View view) {
+        Intent intent = new  Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void handledImplicitIntent(View view) {
+
+    }
 }
